381f3596d302e62b64f2be151da4a049d1e61e24
Shelsy Elaine <elainesbarrera@gmail.com>
2021-09-03 13:12:43 -0700

    FIX: carousel buttons

--- client/src/components/Body/Slides.js
+++ client/src/components/Body/Slides.js
@@ -31,19 +31,23 @@ const Slides = () => {
           ></img>
         </div>
       </div>
+
+
       <button
         className="carousel-control-prev"
         type="button"
-        data-bs-target="#carouselExampleFade"
+        data-bs-target="#carouselExampleInterval"
         data-bs-slide="prev"
       >
         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
         <span className="visually-hidden">Previous</span>
       </button>
+
+
       <button
         className="carousel-control-next"
         type="button"
-        data-bs-target="#carouselExampleFade"
+        data-bs-target="#carouselExampleInterval"
         data-bs-slide="next"
       >
         <span className="carousel-control-next-icon" aria-hidden="true"></span>
