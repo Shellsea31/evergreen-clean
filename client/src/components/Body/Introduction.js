f1f9970a6eb7fc6dc227678f1e7043ca30a5d23b
Shelsy Elaine <elainesbarrera@gmail.com>
2021-09-03 10:33:12 -0700

    ADDED: finished basic styling of intro

--- client/src/components/Body/Introduction.js
+++ client/src/components/Body/Introduction.js
@@ -1,13 +1,18 @@
 import React from "react";
 const cardStyle = {
   minWidth: "auto",
+  padding:"50px"
+};
+
+const introStyle = {
+  fontFamily: "Staatliches, cursive",
 };
 
 const Introduction = () => {
   return (
     <div>
       <div class="card mb-3 bg-transparent border-0" style={cardStyle}>
-        <div class="row g-0">
+        <div class="row">
           {/* Image styling container */}
           <div class="col-sm-4">
             <img
@@ -18,15 +23,10 @@ const Introduction = () => {
           </div>
           <div class="col-sm-6">
             <div class="card-body">
-              <h5 class="card-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Incidunt et ad dolores consequuntur nam dolorem laudantium dicta
-                voluptas pariatur beatae non ea labore nobis, commodi nesciunt
-                molestias quaerat animi libero. Lorem ipsum dolor sit, amet
-                consectetur adipisicing elit. Beatae nam sapiente excepturi
-                quidem quam fuga odit, eius iure, natus sint consequuntur ab,
-                dolore minima? Obcaecati dolorem quaerat id tenetur temporibus.
-              </h5>
+              <h1 class="card-text text-center" style={introStyle}>
+                A company dedicated to helping
+                families achieve their cleaning goals.
+              </h1>
             </div>
           </div>
         </div>
