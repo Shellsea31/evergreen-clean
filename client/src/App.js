5a463d6a0edefd9541bcac91906ea6ef86560ebf
Shelsy Elaine <elainesbarrera@gmail.com>
2021-09-08 12:31:03 -0700

    ADDED: backend server connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
 // import logo from './logo.svg';
 import "./App.css";
 import Body from "./Pages/Body";
+import axios from "axios";
 
 import Footer from "./components/Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Faq from "./Pages/Faq";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get("/example")
+      .then((res) => {
+        console.log(res.data.msg);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
   return (
     <Router>
       <div className="App">
