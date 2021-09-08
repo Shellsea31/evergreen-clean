dc29810376a9b4f0ef4aa50c386cfac68ad49091
Shelsy Elaine <elainesbarrera@gmail.com>
2021-09-08 12:44:38 -0700

    ADDED: comments

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+//building
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
@@ -10,9 +11,11 @@ if (process.env.NODE_ENV === "production") {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// routes
 const apiRoutes = require("./routes/api-routes");
 app.use(apiRoutes);
 
+// connection
 app.listen(PORT, () => {
   console.log(`listening on http://localhost:${PORT}`);
 });
