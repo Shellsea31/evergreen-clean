84c77d049e37f772fc3f21d690918149d844dec7
Shelsy Elaine <elainesbarrera@gmail.com>
2021-09-09 14:25:40 -0700

    FIX: media queries for faq component

--- client/src/components/Body/FAQ.js
+++ client/src/components/Body/FAQ.js
@@ -12,13 +12,6 @@ const FAQ = () => {
     <div>
       <div className="card mb-3 border-0 rounded-0" id="faq" style={cardStyle}>
         <div className="row">
-          <div className="col-md-6 mt-5">
-            <h5 className="card-text text-light">
-              A few of our frequently asked Questions
-            </h5>
-            <i className="bi bi-arrow-right" style={{ fontSize: "50px" }}></i>
-          </div>
-
           <div className="col-md-6">
             <div className="card-body">
               <p>
@@ -29,7 +22,6 @@ const FAQ = () => {
                   role="button"
                   aria-expanded="false"
                   aria-controls="collapseExample1"
-                  
                 >
                   Why should I choose Evergreen Cleaning over your competitors?
                 </a>
@@ -37,8 +29,10 @@ const FAQ = () => {
 
               <div className="collapse text-light" id="collapseExample1">
                 <div className="card card-body bg-transparent border-0">
-                  We offer a trustworthy service unique to your needs. Unlike most standard cleaning service, we are completely flexible and 
-                  encourage our clients to communicate with us whenever something becomes inconvenient. 
+                  We offer a trustworthy service unique to your needs. Unlike
+                  most standard cleaning service, we are completely flexible and
+                  encourage our clients to communicate with us whenever
+                  something becomes inconvenient.
                 </div>
               </div>
               <p>
@@ -55,8 +49,10 @@ const FAQ = () => {
               </p>
               <div className="collapse text-light" id="collapseExample2">
                 <div className="card card-body bg-transparent border-0">
-                  Because we like to cater to your needs and want to be as versatile as possible with all our clients, we ask that
-                  you provide the supplies you believe are safe and best for your home or work space. 
+                  Because we like to cater to your needs and want to be as
+                  versatile as possible with all our clients, we ask that you
+                  provide the supplies you believe are safe and best for your
+                  home or work space.
                 </div>
               </div>
 
@@ -75,12 +71,22 @@ const FAQ = () => {
 
               <div className="collapse text-light" id="collapseExample3">
                 <div className="card card-body bg-transparent border-0">
-                  We love when there is trust between us and our clients. However, we recognize that it's
-                  not immediate. So, we have no problem with you being present at some or all the cleaning sessions. 
+                  We love when there is trust between us and our clients.
+                  However, we recognize that it's not immediate. So, we have no
+                  problem with you being present at some or all the cleaning
+                  sessions.
                 </div>
               </div>
-              <a className="btn btn-light" href="/faq"> View More</a>
             </div>
+          </div>
+          <div className="col-md-6 mt-5">
+            <h5 className="card-text text-light">
+              A few of our frequently asked Questions
+            </h5>
+            <a className="btn btn-light mt-3" href="/faq">
+              {" "}
+              View More
+            </a>
           </div>
         </div>
       </div>
